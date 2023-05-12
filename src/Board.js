e32bc59eb810173f44992e846e48324f9bf28216
tarynries <taryn.ries@gmail.com>
2023-05-12 14:21:33 +0200

    partially done

---
+++ src/Board.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import Cell from "./Cell";
+import "./Board.css";
+
+
+/** Game board of Lights out.
+ *
+ * Properties:
+ *
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ *
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ *
+ **/
+
+function Board({ nrows, ncols, chanceLightStartsOn }) {
+  const [board, setBoard] = useState(createBoard());
+
+  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+  function createBoard() {
+    let initialBoard = [];
+    for (let i = 0; i < nrows; i++) {
+      let row = [];
+      for (let j = 0; j < ncols; j++) {
+        row.push(Math.random() < chanceLightStartsOn);
+      }
+      initialBoard.push(row);
+    }
+    // TODO: create array-of-arrays of true/false values
+    return initialBoard;
+  }
+
+  function hasWon() {
+    return board.every(row => row.every(cell => !cell))
+    // TODO: check the board in state to determine whether the player has won.
+  }
+
+  function flipCellsAround(coord) {
+    setBoard(oldBoard => {
+      const [y, x] = coord.split("-").map(Number);
+
+      const flipCell = (y, x, boardCopy) => {
+        // if this coord is actually on board, flip it
+
+        if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+          boardCopy[y][x] = !boardCopy[y][x];
+        }
+      };
+
+      const boardCopy = JSON.parse(JSON.stringify(oldBoard));
+
+      flipCell(y, x, boardCopy);
+      flipCell(y, x - 1, boardCopy);
+      flipCell(y, x + 1, boardCopy);
+      flipCell(y - 1, x, boardCopy);
+      flipCell(y + 1, x, boardCopy);
+
+      // return boardCopy();
+      return boardCopy;
+      //I have chaged this, may need to change it back to return boardCopy();
+
+      // TODO: Make a (deep) copy of the oldBoard
+
+      // TODO: in the copy, flip this cell and the cells around it
+
+      // TODO: return the copy
+    });
+  }
+
+  if (hasWon()) {
+    return <div>You won!</div>
+  }
+
+  //   let tblBoard = [];
+
+  //   for (let y = 0; y < nrows; y++) {
+  //     let row = [];
+  //     for (let x = 0; x < ncols; x++) {
+  //       let coord = `${y}-${x}`;
+  //       row.push(
+  //         <Cell
+  //           key={coord}
+  //           isLit={board[y][x]}
+  //           flipCellsAroundMe={evt => flipCellsAround(coord)}
+  //         />,
+  //       );
+  //     }
+  //     tblBoard.push(<tr key={y}>{row}</tr>);
+  //   }
+
+  //   return (
+  //     <table className="Board">
+  //       <tbody>{tblBoard}</tbody>
+  //     </table>
+  //   );
+  // }
+
+  // if the game is won, just show a winning msg & render nothing else
+
+  // TODO
+
+  // make table board
+
+  return (
+    < table className="Board" >
+      <tbody>
+        {board.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((cell, colIndex) => (
+              <Cell
+                key={`${rowIndex}-${colIndex}`}
+                isLit={cell}
+                flipCellsAroundMe={() => flipCellsAround(`${rowIndex}-${colIndex}`)}
+              />
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table >
+  );
+}
+
+// TODO
+// }
+
+export default Board;
---
+++ src/Cell.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Cell.css";
+
+/** A single cell on the board.
+ *
+ * This has no state --- just two props:
+ *
+ * - flipCellsAroundMe: a function rec'd from the board which flips this
+ *      cell and the cells around of it
+ *
+ * - isLit: boolean, is this cell lit?
+ *
+ * This handles clicks --- by calling flipCellsAroundMe
+ *
+ **/
+
+function Cell({ flipCellsAroundMe, isLit }) {
+  const classes = `Cell ${isLit ? "Cell-lit" : ""}`;
+  return <div className={classes} onClick={flipCellsAroundMe} />;
+  //the td component could maybe be a div? but i dont think it is necessary 
+}
+
+export default Cell;
